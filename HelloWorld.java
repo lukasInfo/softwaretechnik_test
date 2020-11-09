703c57812ed291448953d4cf55952c129d825cc5
Lukas <lukasduechting@mailbox.org>
2020-11-09 10:37:19 +0100

    print matriculated years

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,20 @@
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.Period;
+
+
 public class HelloWorld 
 {
  
        public static void main (String[] args)
        {
-             // Ausgabe Hello World!
-             System.out.println("Hello World!");
+             LocalDate date_UoC_imma = LocalDate.of(2010, 10, 1);
+             LocalDate date_now = LocalDate.now();
+             
+             Period university_time = date_UoC_imma.until(date_now);
+             
+             
+             System.out.format("Matriculated since %d years, %d month. %n", university_time.get(ChronoUnit.YEARS),university_time.get(ChronoUnit.MONTHS));
        }
 }
